00f0d985d06353d5c3ac5e34fcc8051baf740a62
lnguyen193 <lynn.clover1206@gmail.com>
2020-08-25 10:08:11 -0400

    second commit

--- src/DoctorOffice.java
+++ src/DoctorOffice.java
@@ -3,17 +3,30 @@ public class DoctorOffice {
 
         String patientName = "Stacey Manson";
 
+        String appDate = "5/10/22";
+
+        String contactNum = "939-542-6478";
+
         boolean hasInsurance = false;
 
         double temp = 102.2;
 
+        double copay = 20.00;
+
+
         int numVisits = 5;
 
         System.out.println("Patient Details: ");
+
         System.out.println("Name: " + patientName);
+
         System.out.println("Patient has insurance: " + hasInsurance);
+
         System.out.println("Body temperature: " + temp);
+
         System.out.println("Number of visits: " + numVisits);
+
+        System.out.println("Copay amount: $" + copay + "0");
 
 
     }
