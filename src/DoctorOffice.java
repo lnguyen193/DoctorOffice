c06bf9637ec58dcdcecf1b804d4f673ceac86d46
lnguyen193 <lynn.clover1206@gmail.com>
2020-08-24 22:46:50 -0400

    first commit

---
+++ src/DoctorOffice.java
@@ -0,0 +1,20 @@
+public class DoctorOffice {
+    public static void main(String[] args){
+
+        String patientName = "Stacey Manson";
+
+        boolean hasInsurance = false;
+
+        double temp = 102.2;
+
+        int numVisit = 5;
+
+        System.out.println("Patient Details: ");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature: " + temp);
+        System.out.println("Number of visit: " + numVisit);
+
+
+    }
+}
